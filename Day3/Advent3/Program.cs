266a81400dfd8a3800c476eba90866ea110681f2
Kory Becker <kbecker@primaryobjects.com>
2023-12-17 19:58:40 -0500

    Initial commit.

---
+++ Day3/Advent3/Program.cs
@@ -0,0 +1,15 @@
+﻿using Managers;
+
+// Read schematic input.
+var lines = CommonManager.ReadInput(Path.Combine(Directory.GetCurrentDirectory(), "../../../input.txt"));
+
+// Build a map, including coordinates of the symbols.
+var mapping = CommonManager.Analyze(lines);
+
+// Find all numbers that are adjacent to the symbols.
+var values = CommonManager.FindAdjacent(mapping);
+values.ForEach(Console.WriteLine);
+
+Console.WriteLine(values.Sum());
+
+Console.WriteLine(CommonManager.FindAdjacentGears(mapping).Sum());
